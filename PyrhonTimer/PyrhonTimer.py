35df16ba701b0febb9fb18c10657c07047aa9de6
Янош Строганов <suslik22881@gmail.com>
2021-03-15 09:44:21 +0200

    fix

--- PyrhonTimer/PyrhonTimer.py
+++ PyrhonTimer/PyrhonTimer.py
@@ -1,5 +1,6 @@
 import os
 from tkinter import *
+from tkinter import messagebox
 from datetime import *
 import win10toast as w10t
 
@@ -28,11 +29,14 @@ seconds_checkbutton = Radiobutton(text = "Seconds", value=3, variable=type)
 seconds_checkbutton.pack()
 seconds_checkbutton.place(x=120, y=220)
 
+def show_message():
+    messagebox.showinfo("GUI Python", timeout.get())
+
 def click_button():
-    os.system("shutdown /s /t " + str(timeout))
+    os.system("shutdown /s /t " + str(timeout.get()))
     toaster.show_toast("Shutdown", "Your system will be shut down in few minutes.", duration = 10, threaded = True)
 def click_button1():
-    os.system("shutdown /r")
+    os.system("shutdown /r /t " + str(timeout.get()))
     toaster.show_toast("Reboot", "Your system will be rebooted in few minutes.", duration = 10, threaded = True)
 def click_button2():
     os.system("shutdown /h")
@@ -40,6 +44,10 @@ def click_button2():
 def click_button3():
     os.system("shutdown /a")
     toaster.show_toast("Decline", "You have declined operation.", duration = 10, threaded = True)
+
+message_button = Button(text="Click Me", command=show_message)
+message_button.pack()
+message_button.place(x=20,y=50)
 
 btn = Button(text="Shutdown", command=click_button)
 btn.pack()
